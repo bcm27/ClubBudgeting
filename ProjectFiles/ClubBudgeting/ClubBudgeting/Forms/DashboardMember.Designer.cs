1271a33d21d334c1aacbdb04e09cb53909d30877
Bjorn Mathisen <Bjorn Mathisen>
2017-04-13 18:40:54 -0500

    worked on forms and login screen

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/DashboardMember.Designer.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/DashboardMember.Designer.cs
@@ -28,11 +28,45 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.SuspendLayout();
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(12, 9);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(35, 13);
+         this.label1.TabIndex = 0;
+         this.label1.Text = "label1";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(12, 33);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(35, 13);
+         this.label2.TabIndex = 1;
+         this.label2.Text = "label2";
+         // 
+         // DashboardMember
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(692, 436);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Name = "DashboardMember";
          this.Text = "DashboardMember";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
       }
 
       #endregion
+
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
    }
 }
