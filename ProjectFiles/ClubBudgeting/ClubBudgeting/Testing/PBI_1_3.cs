94f27ac606a8d1c62a6205f882d9ab4714888714
Bjorn Mathisen <bcm27@live.com>
2017-04-12 23:18:40 -0500

    added amandas test files

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Testing/PBI_1_3.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Testing/PBI_1_3.cs
@@ -1,4 +1,6 @@
-﻿using System;
+// PBI 1 and 3 tests
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +13,23 @@ using MySql.Data.MySqlClient;
 namespace ClubBudgeting.Testing
 {
    class PBI_3_1
-   {/*
+   {
       MySqlConnection SQLCONN = new MySqlConnection("server=localhost;"
        + "user=root;database=Club_Funds;port=3306;password=potato123");
       string stmt;
+
       [SetUp]
       public void beforeTests()
       {
          SQLCONN.Open();
       }
+
       [TearDown]
       public void afterTests()
       {
          SQLCONN.Close();
       }
+
       [Test]
       public void PBI_1()
       {
@@ -32,13 +37,16 @@ namespace ClubBudgeting.Testing
          string var = "";
          stmt = "";
          MySqlCommand cmd;
+
          SQL.addClub(null, "Solar Car", "Club builds solar car"); // check Club
+
          try
          {
             stmt = "SELECT * FROM Club WHERE name = 'Solar Car'";
             cmd = new MySqlCommand(stmt, SQLCONN);
             cmd.ExecuteNonQuery();
             rd = cmd.ExecuteReader();
+
             rd.Read();
             var = rd[1].ToString();
             rd.Close();
@@ -47,14 +55,18 @@ namespace ClubBudgeting.Testing
          {
             Assert.True(false);
          }
+
          Assert.True(true);
+
          SQL.addBudget(null, 10000, 5000, 3000, 2000);   // check Budget
+
          try
          {
             stmt = "SELECT * FROM Budget where allocatedMoney = 10000";
             cmd = new MySqlCommand(stmt, SQLCONN);
             cmd.ExecuteNonQuery();
             rd = cmd.ExecuteReader();
+
             rd.Read();
             var = rd[1].ToString();
             rd.Close();
@@ -63,8 +75,10 @@ namespace ClubBudgeting.Testing
          {
             Assert.True(false);
          }
+
          Assert.True(true);
       }
+
       [Test]
       public void PBI_3()
       {
@@ -72,16 +86,19 @@ namespace ClubBudgeting.Testing
          string var = "";
          stmt = "";
          MySqlCommand cmd;
+
          // need to add club and budget before transaction
          SQL.addClub(null, "Solar Car", "Club builds solar car");
          SQL.addBudget(null, 10000, 5000, 3000, 2000);
          SQL.addTransaction("null", "2012-12-12", 300, "Food costs", 2, 1);
+
          try
          {
             stmt = "SELECT * FROM Club WHERE name = 'Solar Car'";
             cmd = new MySqlCommand(stmt, SQLCONN);
             cmd.ExecuteNonQuery();
             rd = cmd.ExecuteReader();
+
             rd.Read();
             var = rd[2].ToString();
             rd.Close();
@@ -90,15 +107,19 @@ namespace ClubBudgeting.Testing
          {
             Assert.True(false);
          }
+
          Assert.AreEqual(var.ToString(), "Solar Car");
+
          try
          {
             // refund so this should fail 
             SQL.addTransaction("null", "2012-12-12", -12000, "Travel", 1, 1, 1);
+
             stmt = "SELECT * FROM Transactions WHERE transactionsType = 1";
             cmd = new MySqlCommand(stmt, SQLCONN);
             cmd.ExecuteNonQuery();
             rd = cmd.ExecuteReader();
+
             rd.Read();
             var = rd[2].ToString();
             rd.Close();
@@ -107,7 +128,8 @@ namespace ClubBudgeting.Testing
          {
             Assert.True(true);   // this transaction should fail
          }
+
          Assert.True(false);  // shouldn't be true
-      }*/
+      }
    }
 }
