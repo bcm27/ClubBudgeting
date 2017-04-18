72201511ffb6d4ce11a632a12ddec853166b223a
Bjorn Mathisen <Bjorn Mathisen>
2017-04-18 10:44:56 -0500

    worked on login and register functionality

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/Login.Designer.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/Login.Designer.cs
@@ -33,6 +33,7 @@
          this.txtbx1_userName = new System.Windows.Forms.TextBox();
          this.txtbx2_password = new System.Windows.Forms.TextBox();
          this.but1_login = new System.Windows.Forms.Button();
+         this.but2_createAccount = new System.Windows.Forms.Button();
          this.SuspendLayout();
          // 
          // lab1_login
@@ -70,19 +71,30 @@
          // 
          // but1_login
          // 
-         this.but1_login.Location = new System.Drawing.Point(283, 149);
+         this.but1_login.Location = new System.Drawing.Point(255, 149);
          this.but1_login.Name = "but1_login";
-         this.but1_login.Size = new System.Drawing.Size(75, 23);
+         this.but1_login.Size = new System.Drawing.Size(103, 23);
          this.but1_login.TabIndex = 4;
          this.but1_login.Text = "Login";
          this.but1_login.UseVisualStyleBackColor = true;
          this.but1_login.Click += new System.EventHandler(this.but1_login_Click);
+         // 
+         // but2_createAccount
+         // 
+         this.but2_createAccount.Location = new System.Drawing.Point(255, 178);
+         this.but2_createAccount.Name = "but2_createAccount";
+         this.but2_createAccount.Size = new System.Drawing.Size(103, 23);
+         this.but2_createAccount.TabIndex = 5;
+         this.but2_createAccount.Text = "Create Account";
+         this.but2_createAccount.UseVisualStyleBackColor = true;
+         this.but2_createAccount.Click += new System.EventHandler(this.but2_createAccount_Click);
          // 
          // Form1
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(550, 261);
+         this.Controls.Add(this.but2_createAccount);
          this.Controls.Add(this.but1_login);
          this.Controls.Add(this.txtbx2_password);
          this.Controls.Add(this.txtbx1_userName);
@@ -102,6 +114,7 @@
       private System.Windows.Forms.TextBox txtbx1_userName;
       private System.Windows.Forms.TextBox txtbx2_password;
       private System.Windows.Forms.Button but1_login;
+      private System.Windows.Forms.Button but2_createAccount;
    }
 }
 
