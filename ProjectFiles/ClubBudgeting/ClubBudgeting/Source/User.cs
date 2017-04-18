1f88edb23c226e0989eeebfaadfaf3ede69e48b8
Sam Meier <samuel.p.meier@gmail.com>
2017-04-18 09:56:29 -0500

    stuff

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Source/User.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Source/User.cs
@@ -93,7 +93,7 @@ namespace ClubBudgeting
       {
          try
          {
-            Tuple<bool, string> temp = sql.logIn(name, pass);
+            string temp = sql.logIn(name, pass);
             username = name;
             admin = temp.Item1;
             club = temp.Item2;
