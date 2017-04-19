940393374138add67f3a64599ad3b31b84a9fb54
Amanda Breneman <amanda.breneman@principia.edu>
2017-04-18 22:46:45 -0500

    Fixes

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/DashboardAdmin.Designer.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/DashboardAdmin.Designer.cs
@@ -48,6 +48,9 @@
          this.label4 = new System.Windows.Forms.Label();
          this.lab_transStatus = new System.Windows.Forms.Label();
          this.lab_clubStatus = new System.Windows.Forms.Label();
+         this.lab_debt = new System.Windows.Forms.Label();
+         this.lab_totalDebt = new System.Windows.Forms.Label();
+         this.but_viewTransactions = new System.Windows.Forms.Button();
          this.SuspendLayout();
          // 
          // lab1_acctName
@@ -55,9 +58,9 @@
          this.lab1_acctName.AutoSize = true;
          this.lab1_acctName.Location = new System.Drawing.Point(36, 35);
          this.lab1_acctName.Name = "lab1_acctName";
-         this.lab1_acctName.Size = new System.Drawing.Size(158, 25);
+         this.lab1_acctName.Size = new System.Drawing.Size(124, 25);
          this.lab1_acctName.TabIndex = 0;
-         this.lab1_acctName.Text = "Account Name:";
+         this.lab1_acctName.Text = "Club Name:";
          // 
          // lab3_budgetTotal
          // 
@@ -71,7 +74,7 @@
          // lab4_purchaseAmt
          // 
          this.lab4_purchaseAmt.AutoSize = true;
-         this.lab4_purchaseAmt.Location = new System.Drawing.Point(39, 221);
+         this.lab4_purchaseAmt.Location = new System.Drawing.Point(39, 253);
          this.lab4_purchaseAmt.Name = "lab4_purchaseAmt";
          this.lab4_purchaseAmt.Size = new System.Drawing.Size(276, 25);
          this.lab4_purchaseAmt.TabIndex = 3;
@@ -80,7 +83,7 @@
          // lab5_refundAmt
          // 
          this.lab5_refundAmt.AutoSize = true;
-         this.lab5_refundAmt.Location = new System.Drawing.Point(36, 277);
+         this.lab5_refundAmt.Location = new System.Drawing.Point(36, 309);
          this.lab5_refundAmt.Name = "lab5_refundAmt";
          this.lab5_refundAmt.Size = new System.Drawing.Size(265, 25);
          this.lab5_refundAmt.TabIndex = 4;
@@ -88,14 +91,14 @@
          // 
          // txtbx1_transAmt
          // 
-         this.txtbx1_transAmt.Location = new System.Drawing.Point(319, 218);
+         this.txtbx1_transAmt.Location = new System.Drawing.Point(319, 250);
          this.txtbx1_transAmt.Name = "txtbx1_transAmt";
          this.txtbx1_transAmt.Size = new System.Drawing.Size(192, 31);
          this.txtbx1_transAmt.TabIndex = 6;
          // 
          // txtbx2_date
          // 
-         this.txtbx2_date.Location = new System.Drawing.Point(319, 274);
+         this.txtbx2_date.Location = new System.Drawing.Point(319, 306);
          this.txtbx2_date.Name = "txtbx2_date";
          this.txtbx2_date.Size = new System.Drawing.Size(192, 31);
          this.txtbx2_date.TabIndex = 8;
@@ -112,7 +115,7 @@
          // lab_clubName
          // 
          this.lab_clubName.AutoSize = true;
-         this.lab_clubName.Location = new System.Drawing.Point(200, 35);
+         this.lab_clubName.Location = new System.Drawing.Point(166, 35);
          this.lab_clubName.Name = "lab_clubName";
          this.lab_clubName.Size = new System.Drawing.Size(115, 25);
          this.lab_clubName.TabIndex = 11;
@@ -129,18 +132,18 @@
          // 
          // but1_submitTransaction
          // 
-         this.but1_submitTransaction.Location = new System.Drawing.Point(575, 218);
+         this.but1_submitTransaction.Location = new System.Drawing.Point(575, 250);
          this.but1_submitTransaction.Name = "but1_submitTransaction";
          this.but1_submitTransaction.Size = new System.Drawing.Size(169, 87);
          this.but1_submitTransaction.TabIndex = 14;
-         this.but1_submitTransaction.Text = "Submit transaction";
+         this.but1_submitTransaction.Text = "Submit Transaction";
          this.but1_submitTransaction.UseVisualStyleBackColor = true;
          this.but1_submitTransaction.Click += new System.EventHandler(this.but1_submitTransaction_Click);
          // 
          // label1
          // 
          this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(333, 321);
+         this.label1.Location = new System.Drawing.Point(333, 353);
          this.label1.Name = "label1";
          this.label1.Size = new System.Drawing.Size(152, 25);
          this.label1.TabIndex = 15;
@@ -148,24 +151,24 @@
          // 
          // but_addClub
          // 
-         this.but_addClub.Location = new System.Drawing.Point(575, 440);
+         this.but_addClub.Location = new System.Drawing.Point(575, 472);
          this.but_addClub.Name = "but_addClub";
          this.but_addClub.Size = new System.Drawing.Size(169, 92);
          this.but_addClub.TabIndex = 20;
-         this.but_addClub.Text = "Add club";
+         this.but_addClub.Text = "Add Club";
          this.but_addClub.UseVisualStyleBackColor = true;
          this.but_addClub.Click += new System.EventHandler(this.but_addClub_Click);
          // 
          // txtbx_clubDesc
          // 
-         this.txtbx_clubDesc.Location = new System.Drawing.Point(319, 501);
+         this.txtbx_clubDesc.Location = new System.Drawing.Point(319, 533);
          this.txtbx_clubDesc.Name = "txtbx_clubDesc";
          this.txtbx_clubDesc.Size = new System.Drawing.Size(192, 31);
          this.txtbx_clubDesc.TabIndex = 19;
          // 
          // txtbx_clubName
          // 
-         this.txtbx_clubName.Location = new System.Drawing.Point(319, 440);
+         this.txtbx_clubName.Location = new System.Drawing.Point(319, 472);
          this.txtbx_clubName.Name = "txtbx_clubName";
          this.txtbx_clubName.Size = new System.Drawing.Size(192, 31);
          this.txtbx_clubName.TabIndex = 18;
@@ -173,7 +176,7 @@
          // label2
          // 
          this.label2.AutoSize = true;
-         this.label2.Location = new System.Drawing.Point(39, 501);
+         this.label2.Location = new System.Drawing.Point(39, 533);
          this.label2.Name = "label2";
          this.label2.Size = new System.Drawing.Size(244, 25);
          this.label2.TabIndex = 17;
@@ -182,7 +185,7 @@
          // lab_
          // 
          this.lab_.AutoSize = true;
-         this.lab_.Location = new System.Drawing.Point(39, 443);
+         this.lab_.Location = new System.Drawing.Point(39, 475);
          this.lab_.Name = "lab_";
          this.lab_.Size = new System.Drawing.Size(192, 25);
          this.lab_.TabIndex = 16;
@@ -192,7 +195,7 @@
          // 
          this.label3.AutoSize = true;
          this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.label3.Location = new System.Drawing.Point(39, 381);
+         this.label3.Location = new System.Drawing.Point(39, 413);
          this.label3.Name = "label3";
          this.label3.Size = new System.Drawing.Size(143, 29);
          this.label3.TabIndex = 21;
@@ -202,7 +205,7 @@
          // 
          this.label4.AutoSize = true;
          this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.label4.Location = new System.Drawing.Point(39, 162);
+         this.label4.Location = new System.Drawing.Point(39, 194);
          this.label4.Name = "label4";
          this.label4.Size = new System.Drawing.Size(221, 29);
          this.label4.TabIndex = 22;
@@ -211,7 +214,7 @@
          // lab_transStatus
          // 
          this.lab_transStatus.AutoSize = true;
-         this.lab_transStatus.Location = new System.Drawing.Point(763, 257);
+         this.lab_transStatus.Location = new System.Drawing.Point(763, 289);
          this.lab_transStatus.Name = "lab_transStatus";
          this.lab_transStatus.Size = new System.Drawing.Size(73, 25);
          this.lab_transStatus.TabIndex = 23;
@@ -220,17 +223,48 @@
          // lab_clubStatus
          // 
          this.lab_clubStatus.AutoSize = true;
-         this.lab_clubStatus.Location = new System.Drawing.Point(763, 480);
+         this.lab_clubStatus.Location = new System.Drawing.Point(763, 512);
          this.lab_clubStatus.Name = "lab_clubStatus";
          this.lab_clubStatus.Size = new System.Drawing.Size(73, 25);
          this.lab_clubStatus.TabIndex = 24;
          this.lab_clubStatus.Text = "Status";
+         // 
+         // lab_debt
+         // 
+         this.lab_debt.AutoSize = true;
+         this.lab_debt.Location = new System.Drawing.Point(36, 127);
+         this.lab_debt.Name = "lab_debt";
+         this.lab_debt.Size = new System.Drawing.Size(117, 25);
+         this.lab_debt.TabIndex = 25;
+         this.lab_debt.Text = "Total Debt:";
+         // 
+         // lab_totalDebt
+         // 
+         this.lab_totalDebt.AutoSize = true;
+         this.lab_totalDebt.Location = new System.Drawing.Point(159, 127);
+         this.lab_totalDebt.Name = "lab_totalDebt";
+         this.lab_totalDebt.Size = new System.Drawing.Size(54, 25);
+         this.lab_totalDebt.TabIndex = 26;
+         this.lab_totalDebt.Text = "debt";
+         // 
+         // but_viewTransactions
+         // 
+         this.but_viewTransactions.Location = new System.Drawing.Point(742, 35);
+         this.but_viewTransactions.Name = "but_viewTransactions";
+         this.but_viewTransactions.Size = new System.Drawing.Size(169, 87);
+         this.but_viewTransactions.TabIndex = 27;
+         this.but_viewTransactions.Text = "View Transactions";
+         this.but_viewTransactions.UseVisualStyleBackColor = true;
+         this.but_viewTransactions.Click += new System.EventHandler(this.but_viewTransactions_Click);
          // 
          // DashboardAdmin
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(988, 719);
+         this.Controls.Add(this.but_viewTransactions);
+         this.Controls.Add(this.lab_totalDebt);
+         this.Controls.Add(this.lab_debt);
          this.Controls.Add(this.lab_clubStatus);
          this.Controls.Add(this.lab_transStatus);
          this.Controls.Add(this.label4);
@@ -280,5 +314,8 @@
       private System.Windows.Forms.Label label4;
       private System.Windows.Forms.Label lab_transStatus;
       private System.Windows.Forms.Label lab_clubStatus;
+      private System.Windows.Forms.Label lab_debt;
+      private System.Windows.Forms.Label lab_totalDebt;
+      private System.Windows.Forms.Button but_viewTransactions;
    }
 }
