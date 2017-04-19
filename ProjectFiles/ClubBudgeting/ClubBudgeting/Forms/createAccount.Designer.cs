27b4b1b5c6d81d7841a7a220d0cc7e111795d7bb
Bjorn Mathisen <bcm27@live.com>
2017-04-18 22:09:37 -0500

    added createAccount Form

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/createAccount.Designer.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/createAccount.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ClubBudgeting.Forms
 {
-   partial class createAccount
+   partial class CreateAccount
    {
       /// <summary>
       /// Required designer variable.
@@ -39,8 +39,8 @@
          this.lab6_clubID = new System.Windows.Forms.Label();
          this.textBox1 = new System.Windows.Forms.TextBox();
          this.textBox2 = new System.Windows.Forms.TextBox();
-         this.textBox3 = new System.Windows.Forms.TextBox();
          this.but1_register = new System.Windows.Forms.Button();
+         this.comboBox_clublist = new System.Windows.Forms.ComboBox();
          this.SuspendLayout();
          // 
          // lab1_account
@@ -132,13 +132,6 @@
          this.textBox2.Size = new System.Drawing.Size(100, 20);
          this.textBox2.TabIndex = 10;
          // 
-         // textBox3
-         // 
-         this.textBox3.Location = new System.Drawing.Point(302, 93);
-         this.textBox3.Name = "textBox3";
-         this.textBox3.Size = new System.Drawing.Size(100, 20);
-         this.textBox3.TabIndex = 11;
-         // 
          // but1_register
          // 
          this.but1_register.Location = new System.Drawing.Point(428, 93);
@@ -147,14 +140,25 @@
          this.but1_register.TabIndex = 12;
          this.but1_register.Text = "Register";
          this.but1_register.UseVisualStyleBackColor = true;
+         this.but1_register.Click += new System.EventHandler(this.but1_register_Click);
          // 
-         // createAccount
+         // comboBox_clublist
+         // 
+         this.comboBox_clublist.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+         this.comboBox_clublist.FormattingEnabled = true;
+         this.comboBox_clublist.Location = new System.Drawing.Point(302, 90);
+         this.comboBox_clublist.Name = "comboBox_clublist";
+         this.comboBox_clublist.Size = new System.Drawing.Size(100, 21);
+         this.comboBox_clublist.TabIndex = 14;
+         this.comboBox_clublist.SelectedIndexChanged += new System.EventHandler(this.comboBox_clublist_SelectedIndexChanged);
+         // 
+         // CreateAccount
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(518, 129);
+         this.Controls.Add(this.comboBox_clublist);
          this.Controls.Add(this.but1_register);
-         this.Controls.Add(this.textBox3);
          this.Controls.Add(this.textBox2);
          this.Controls.Add(this.textBox1);
          this.Controls.Add(this.lab6_clubID);
@@ -166,7 +170,7 @@
          this.Controls.Add(this.lab3_password_check);
          this.Controls.Add(this.lab2_password);
          this.Controls.Add(this.lab1_account);
-         this.Name = "createAccount";
+         this.Name = "CreateAccount";
          this.Text = "createAccount";
          this.ResumeLayout(false);
          this.PerformLayout();
@@ -186,7 +190,7 @@
       private System.Windows.Forms.Label lab6_clubID;
       private System.Windows.Forms.TextBox textBox1;
       private System.Windows.Forms.TextBox textBox2;
-      private System.Windows.Forms.TextBox textBox3;
       private System.Windows.Forms.Button but1_register;
+      private System.Windows.Forms.ComboBox comboBox_clublist;
    }
 }
