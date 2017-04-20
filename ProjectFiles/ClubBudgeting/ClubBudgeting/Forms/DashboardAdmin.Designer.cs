4f28f90689d7948d8f92ead4e9f987de739a5082
Amanda Breneman <amanda.breneman@principia.edu>
2017-04-20 15:09:07 -0500

    Fixed comments and style issues

--- ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/DashboardAdmin.Designer.cs
+++ ProjectFiles/ClubBudgeting/ClubBudgeting/Forms/DashboardAdmin.Designer.cs
@@ -202,7 +202,6 @@
          this.button1.TabIndex = 28;
          this.button1.Text = "Create Account";
          this.button1.UseVisualStyleBackColor = true;
-         this.button1.Click += new System.EventHandler(this.button1_Click);
          // 
          // DashboardAdmin
          // 
